a5b080ee3262bc994c6eccb8f4f95fb99f594bd8
Antoine Brassard <antoine.brassard@gmail.com>
2018-11-26 18:12:06 -0500

    fix entete pas tjrs bonne

--- GGFlix/App_Code/Securite.cs
+++ GGFlix/App_Code/Securite.cs
@@ -9,14 +9,12 @@ using LibrairieBD.Entites;
 public static class Securite
 {
     private static GenericDao<Utilisateur> daoUtil = Persistance.GetDao<Utilisateur>();
-    private static Utilisateur _utilisateur;
 
     public static Utilisateur UtilisateurCourant{
         get
         {
             string username = HttpContext.Current.User.Identity.Name;
-            return _utilisateur ??
-                   (_utilisateur = daoUtil.Find(new Utilisateur {NomUtilisateur = username })[0]);
+            return daoUtil.Find(new Utilisateur {NomUtilisateur = username })[0];
         }
     }
 }
