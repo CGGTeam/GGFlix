979f33d29de69121d297232d08aad0b04794a585
Franc <Franc@LAPTOP-FP>
2018-12-11 01:41:25 -0500

    Person Done

--- GGFlix/Pages/PersonnalisationEnv.aspx
+++ GGFlix/Pages/PersonnalisationEnv.aspx
@@ -1,52 +1,84 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/Fragments/PageMaitre.master" AutoEventWireup="true" CodeFile="PersonnalisationEnv.aspx.cs" Inherits="Pages_PersonnalisationEnv" %>
 
 <asp:Content runat="server" ContentPlaceHolderID="Head">
+        <script language="javascript" type="text/ecmascript">
+
+
+       function confirmerAppropriation() {
+        var Result = confirm("Êtes-vous certain de vouloir vous appropriez ce dvd?");
+           if (Result == true) {
+               __doPostBack('txtConfirmRetour', "Approprier"); //sending back to server.
+            return true;
+        }
+        else 
+        {
+
+             return false;
+        }
+    }
+
+
+    </script>
 </asp:Content>
-<asp:Content runat="server" ContentPlaceHolderID="ContenuPrincipal">
-    <div class="vertical-center">
+<asp:Content runat="server"  ContentPlaceHolderID="ContenuPrincipal">
+    <asp:Panel runat="server" ID="MainContent" class="vertical-center">
         <div class="container">
             <div class="row flex-column h-100 justify-content-center align-items-center">
                 <div class="jumbotron col-11 col-sm-12 col-md-9 col-lg-7 col-xl-6">
                      <div class="form-group">
                         <h2 class="text-center">Personnalisation de l'environnement</h2>
                     </div>
-                    <div class="form-group ">
-                        <label for="tbCourriel">Courriel : </label>
-                        <asp:TextBox ID="tbCourriel" runat="server" Text="DVD@wesh.qc.ca" class="form-control"/>
+                    <div class="form-group ">                      
                         <br />
-                        <br />
-                        <label for="tbNom">Nom : </label>
-                        <asp:TextBox ID="tbNom" runat="server" Text="Victor Maertens" class="form-control"/>
-                        <br />
-                        <br />
-                        <label for="tbMdeP">Mot de passe : </label>
-                        <asp:TextBox ID="tbMdeP" runat="server" Text="Password1" class="form-control" />
+                        <asp:Label for="tbMdeP">Mot de passe : </asp:Label>
+                        <asp:TextBox ID="tbMdeP" runat="server" TextMode="Password" Text="" class="form-control" />                        
+                        <asp:RegularExpressionValidator Display = "Dynamic" ControlToValidate = "tbMdeP" ID="regLengthPass" 
+                            ValidationExpression = "^\d{5}$" ForeColor="Red" 
+                            runat="server" ErrorMessage="Le mot de passe est uniquement composé de 5 chiffres. <br> Entre 00000 et 99999. "></asp:RegularExpressionValidator>
+                        <div class="text-center">
+                              <br /> 
+                            <asp:Button ID="btnModifier" runat="server"  Text="Modifier mot de passe" class="text-center"/>
+                        </div>
                         <br />
                         <br />                             
-                        <asp:CheckBox ID="cbCourrielAjout" runat="server" Checked="true"  Text="Recevoir un courriel lors d'ajout de DVD" />
+                        <asp:CheckBox ID="cbCourrielAjout" runat="server" AutoPostBack="true"  Text="Recevoir un courriel lors d'ajout de DVD" />
                         <br />
                         <br />                     
-                        <asp:CheckBox ID="cbCourrielRetrait" runat="server" Checked="true"  Text="Recevoir un courriel lors du retrait d'un DVD" />
+                        <asp:CheckBox ID="cbCourrielRetrait" runat="server" AutoPostBack="true"   Text="Recevoir un courriel lors du retrait d'un DVD" />
                         <br />
                         <br />                        
-                        <asp:CheckBox ID="cbCourrielApprop" runat="server" Checked="true"  Text="Recevoir un courriel lors de l'appropriation d'un DVD" />
+                        <asp:CheckBox ID="cbCourrielApprop" runat="server" AutoPostBack="true" Text="Recevoir un courriel lors de l'appropriation d'un DVD" />
                         <br />
                         <br />  
-                        <label for="ddlNbDvdAffiche">Nombre de DVDs à afficher par page : </label>
-                           <asp:DropDownList id="ddlNbDvdAffiche" runat="server">
-                             <asp:ListItem Text="10" Value="0" />
-                             <asp:ListItem Text="15" Value="1" />
-                             <asp:ListItem Text="20" Value="2" />
-                            <asp:ListItem Text="25" Value="3" />
+
+                        <asp:Label >Choisir une image de fond : </asp:Label>&nbsp; 
+                         <asp:FileUpload ID="FileUpload" runat="server"/>       <br />   
+                          <div class="text-center">
+                              <br />
+                            <asp:Button ID="btnUpload" CssClass="btn btn-primary" runat="server" Text="Téléverser" />
+                        </div>                       
+                        <asp:CheckBox ID="cbRetirerImg" runat="server" AutoPostBack="true"  Visible="false"  Text="Voulez vous retirer votre image actuel " />    <br />  <br /> 
+                          
+                         <asp:Label >Choisir une couleur de fond : </asp:Label> &nbsp;  
+                        <asp:DropDownList ID="ddlCouleurFond" AutoPostBack="true"  runat="server">
+                            
+                        </asp:DropDownList>
+                        <br />
+                        <br /> 
+                         <label >Choisir une couleur de texte : </label>&nbsp; 
+                         <asp:DropDownList ID="ddlCouleurTexte" AutoPostBack="true"  runat="server">
+                           
+                        </asp:DropDownList>
+                        <br /><br />
+                        <asp:Label for="ddlNbDvdAffiche">Nombre de DVDs à afficher par page : </asp:Label>
+                           <asp:DropDownList id="ddlNbDvdAffiche" AutoPostBack="true"  runat="server">                            
                          </asp:DropDownList>  
                         <br />
                         <br />  
-                        <div class="text-center">
-                            <asp:Button ID="btnModifier" runat="server" Text="Modifier" class="text-center"/>
-                        </div>
+                        
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </asp:Panel>
 </asp:Content>
--- GGFlix/Pages/PersonnalisationEnv.aspx.cs
+++ GGFlix/Pages/PersonnalisationEnv.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibrairieBD.Dao;
+using LibrairieBD.Entites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,235 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_PersonnalisationEnv : System.Web.UI.Page
 {
+    private GenericDao<Utilisateur> utilDao = Persistance.GetDao<Utilisateur>();
+    private GenericDao<ValeurPreference> valeurPrefDao = Persistance.GetDao<ValeurPreference>();
+    Utilisateur currentUser = null;
+    private String id = "";
+    private string strCBApprop = "";
     protected void Page_Load(object sender, EventArgs e)
     {
+        id = HttpContext.Current.User.Identity.Name;
+        currentUser = utilDao.Find(new Utilisateur { NomUtilisateur = id })[0];
+        if (Page.IsPostBack)
+        {     
+
+            List<ValeurPreference> laValeurEnvoiAjout = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(3)).ToList();
+            if (laValeurEnvoiAjout.Count > 0)
+            {                    
+                ValeurPreference valeurPref = laValeurEnvoiAjout.First();
+                valeurPref.Valeur = cbCourrielAjout.Checked.ToString();
+                valeurPrefDao.Save(valeurPref);
+            }
+            else
+            {                    
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 3, Valeur = cbCourrielAjout.Checked.ToString() };
+                valeurPrefDao.Save(valeurPref);
+            }
+            
+
+            List<ValeurPreference> laValeurEnvoiApprop = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(4)).ToList();
+            if (laValeurEnvoiApprop.Count > 0)
+            {               
+                ValeurPreference valeurPref = laValeurEnvoiApprop.First();
+                valeurPref.Valeur = cbCourrielApprop.Checked.ToString();
+                valeurPrefDao.Save(valeurPref);               
+            }
+            else
+            {               
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 4, Valeur = cbCourrielApprop.Checked.ToString() };
+                valeurPrefDao.Save(valeurPref);                
+            }
+            List<ValeurPreference> laValeurEnvoiSupp = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(5)).ToList();
+            if (laValeurEnvoiSupp.Count > 0)
+            {                
+                ValeurPreference valeurPref = laValeurEnvoiSupp.First();
+                valeurPref.Valeur = cbCourrielRetrait.Checked.ToString();
+                valeurPrefDao.Save(valeurPref);
+            }
+            else
+            {                
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 5, Valeur = cbCourrielRetrait.Checked.ToString() };
+                valeurPrefDao.Save(valeurPref);
+            }
+
+            if (cbRetirerImg.Checked)
+            {
+                List<ValeurPreference> laValeurImageBackground = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(6)).ToList();
+                if (laValeurImageBackground.Count > 0)
+                {
+                    ValeurPreference valeurPref = laValeurImageBackground.First();
+                    valeurPref.Valeur = "";
+                    valeurPrefDao.Save(valeurPref);
+                }
+            }
+          
+            List<ValeurPreference> lesValeursPrefs = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(7)).ToList();
+            if (lesValeursPrefs.Count > 0)
+            {
+                ValeurPreference valeurPref = lesValeursPrefs.First();
+                valeurPref.Valeur = ddlNbDvdAffiche.SelectedValue;
+                valeurPrefDao.Save(valeurPref);
+
+            }
+            else
+            {
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 7, Valeur = ddlNbDvdAffiche.SelectedValue };
+                valeurPrefDao.Save(valeurPref);
+            }
+
+
+            List<ValeurPreference> laValeurCouleurFond = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(1)).ToList();
+            if (laValeurCouleurFond.Count > 0)
+            {                
+                ValeurPreference valeurPref = laValeurCouleurFond.First();
+                valeurPref.Valeur = ddlCouleurFond.SelectedValue;
+                valeurPrefDao.Save(valeurPref);
+            }
+            else
+            {
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 1, Valeur = ddlCouleurFond.SelectedValue };
+                valeurPrefDao.Save(valeurPref);
+            }
+
+
+            List<ValeurPreference> laValeurCouleurTexte = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(2)).ToList();
+            if (laValeurCouleurTexte.Count > 0)
+            {
+                ValeurPreference valeurPref = laValeurCouleurTexte.First();
+                valeurPref.Valeur = ddlCouleurTexte.SelectedValue;
+                valeurPrefDao.Save(valeurPref);
+
+            }
+            else
+            {
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 2, Valeur = ddlCouleurTexte.SelectedValue };
+                valeurPrefDao.Save(valeurPref);
+            }
+
+
+        }
+
+
+        remplireDDLCouleur(ddlCouleurFond);
+        remplireDDLCouleur(ddlCouleurTexte);
+        remplirNbPages(ddlNbDvdAffiche);
+        btnModifier.Click += new EventHandler(validationPreferences);
+        btnUpload.Click += new EventHandler(afficherImage_Click);
+        lesValeursEnvironnement();
+        
+
+     
+    }  
+
+    private void validationPreferences(object sender, EventArgs e)
+    {
+       //if(btnModifier.Click)
 
     }
+    private void afficherImage_Click(object sender, EventArgs e)
+    {
+        if (FileUpload.HasFile)
+        {
+            FileUpload.SaveAs(MapPath("/Static/img/" + FileUpload.FileName));
+            List<ValeurPreference> laValeurImageBackground = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(6)).ToList();
+            if (laValeurImageBackground.Count > 0)
+            {
+                ValeurPreference valeurPref = laValeurImageBackground.First();
+                valeurPref.Valeur = FileUpload.FileName;
+                valeurPrefDao.Save(valeurPref);
+            }
+            else
+            {
+                ValeurPreference valeurPref = new ValeurPreference { NoUtilisateur = currentUser.NoUtilisateur, NoPreference = 6, Valeur = FileUpload.FileName };
+                valeurPrefDao.Save(valeurPref);
+            }
+            lesValeursEnvironnement();
+        }
+    }
+    
+
+    protected void lesValeursEnvironnement()
+    {
+        tbMdeP.Attributes.Add("value", currentUser.MotPasse.ToString());
+
+        List<ValeurPreference> lesValeursPrefs = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(7)).ToList();
+        if (lesValeursPrefs.Count > 0)
+        {
+            ddlNbDvdAffiche.SelectedValue = lesValeursPrefs.First().Valeur;
+        }
+        else
+            ddlNbDvdAffiche.SelectedValue = "10";
+
+        List<ValeurPreference> laValeurImageBackground = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(6)).ToList();
+        List<ValeurPreference> laValeurCouleurFond = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(1)).ToList();
+        if (laValeurImageBackground.Count > 0 && laValeurImageBackground.First().Valeur != "")
+        {
+            MainContent.Attributes.Add("style", " background-image: url('" + "/Static/img/" + laValeurImageBackground.First().Valeur + "');");           
+            ddlCouleurFond.SelectedValue = laValeurCouleurFond.First().Valeur;
+            cbRetirerImg.Visible = true;
+            cbRetirerImg.Checked = false;
+        }
+        else
+        {            
+            if (laValeurCouleurFond.Count > 0)
+            {
+                ddlCouleurFond.SelectedValue = laValeurCouleurFond.First().Valeur;
+                MainContent.Attributes.Add("style","background-color:"+ ddlCouleurFond.SelectedValue);
+            }
+            cbRetirerImg.Visible = false;
+        }
+
+        List<ValeurPreference> laValeurCouleurTexte = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(2)).ToList();
+        if (laValeurCouleurTexte.Count > 0)
+        {
+            MainContent.Style.Add("color", laValeurCouleurTexte.First().Valeur);
+            ddlCouleurTexte.SelectedValue = laValeurCouleurTexte.First().Valeur;
+        }
+
+        List<ValeurPreference> laValeurEnvoiAjout = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(3)).ToList();
+        if (laValeurEnvoiAjout.Count > 0)
+        {
+            string strChecked = laValeurEnvoiAjout.First().Valeur.ToString() == "1" ? "true" : "false";
+            cbCourrielAjout.Checked = Convert.ToBoolean(strChecked);
+        }
+
+        List<ValeurPreference> laValeurEnvoiApprop = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(4)).ToList();
+        if (laValeurEnvoiApprop.Count > 0)
+        {
+            string strChecked = laValeurEnvoiApprop.First().Valeur.ToString() == "1" ? "true" : "false";
+            cbCourrielApprop.Checked = Convert.ToBoolean(strChecked);
+        }
+
+        List<ValeurPreference> laValeurEnvoiSupp = valeurPrefDao.FindAll().Where(v => v.NoUtilisateur.Equals(currentUser.NoUtilisateur) && v.NoPreference.Equals(5)).ToList();
+        if (laValeurEnvoiSupp.Count > 0)
+        {
+            string strChecked = laValeurEnvoiSupp.First().Valeur.ToString() == "1" ? "true" : "false";
+            cbCourrielRetrait.Checked = Convert.ToBoolean(strChecked);
+        }
+
+
+    }
+
+
+
+protected void remplireDDLCouleur(DropDownList dropDownList)
+    {
+        dropDownList.Items.Clear();
+        dropDownList.Items.Insert(0, new ListItem("Noir", "Black"));
+        dropDownList.Items.Insert(1, new ListItem("Vert", "Green"));
+        dropDownList.Items.Insert(2, new ListItem("Bleu", "Blue"));
+        dropDownList.Items.Insert(3, new ListItem("Rouge", "Red"));
+        dropDownList.Items.Insert(3, new ListItem("Jaune", "Yellow"));
+    }
+
+    protected void remplirNbPages(DropDownList dropDownList)
+    {
+        for(int i =1 ; i <= 20; i++)
+        {
+            string strValeurs = (i*5).ToString();            
+            dropDownList.Items.Insert(i-1, new ListItem(strValeurs, strValeurs));
+        }
+    }
+
+ 
 }
